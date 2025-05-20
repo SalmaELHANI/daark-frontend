669649cc0c60fcd87fbe395ea22c421811da4bc0
Hibanj8 <hibanaji137@gmail.com>
2025-05-20 12:46:09 +0100

    Initial commit du projet Daark.ma

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen bg-yellow-100 flex items-center justify-center">
+        <h1 className="text-5xl font-bold text-red-600 underline">
+          Hello Daark.ma
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
